f8edab2e8c2a8930ae3a19625d62106cc15d049e
foxane <65512187+foxane@users.noreply.github.com>
2024-04-20 10:29:59 +0700

    Updated layout snad style

--- script.js
+++
@@ -1,6 +0,0 @@
-const colorInput = document.querySelector('#color');
-const newSketchBtn = document.querySelector('#new-sketch');
-
-newSketchBtn.addEventListener('click', function () {
-  console.log(colorInput.value);
-});
