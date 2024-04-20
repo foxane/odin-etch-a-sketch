2f21011b0ebf24ba09b5c2fca727ea382b8aca10
foxane <65512187+foxane@users.noreply.github.com>
2024-04-20 09:32:32 +0700

    Added color picker
    Using built-in html color picker input and custom built rainbow

---
+++ script.js
@@ -0,0 +1,6 @@
+const colorInput = document.querySelector('#color');
+const newSketchBtn = document.querySelector('#new-sketch');
+
+newSketchBtn.addEventListener('click', function () {
+  console.log(colorInput.value);
+});
